4ec1cfda7249245a591e9acf04efcc8a8df2db2a
agauniyal <agauniyal@outlook.com>
2017-06-15 19:50:08 +0530

    Add integration testing for Edit Channel route

--- tests/routes/channelRoutes.test.js
+++ tests/routes/channelRoutes.test.js
@@ -10,6 +10,7 @@ describe('[Integration]: List all channels', () => {
   test('Without setting db', () => {
     return request(app).get('/').expect(500);
   });
+
   test('With correct db', () => {
     db.setDB([]);
     return request(app).get('/').expect(200);
@@ -21,13 +22,14 @@ describe('[Integration]: Get one channel', () => {
     db.setDB([{ name: 'C1', url: 'url1' }]);
     return request(app).get('/url1').expect(200);
   });
+
   test('Channel does not exists', () => {
     db.setDB([{ name: 'C1', url: 'url1' }]);
     return request(app).get('/u4').expect(400);
   });
 });
 
-describe('[Integration]: Add one channel', () => {
+describe('[Integration]: Add a channel', () => {
   test('success', () => {
     db.setDB([]);
     return request(app)
@@ -36,8 +38,47 @@ describe('[Integration]: Add one channel', () => {
       .expect(200)
       .expect('Content-Type', /json/);
   });
+
   test('no channelName sent', () => {
     db.setDB([]);
     return request(app).post('/').expect(400);
+  });
+});
+
+describe('[Integration]: Edit a channel', () => {
+  test('success', () => {
+    db.setDB([]);
+
+    return request(app)
+      .post('/')
+      .send({ channelName: 'myChannel' })
+      .then(res => {
+        const editReq = {
+          channelNewName: 'NewName123',
+          channelUrl: res.body.channelUrl,
+          channelPassword: res.body.channelPassword
+        };
+        return request(app).put('/').send(editReq).expect(200);
+      });
+  });
+
+  test('wrong auth', () => {
+    db.setDB([]);
+    return request(app)
+      .post('/')
+      .send({ channelName: 'myChannel' })
+      .then(res => {
+        const editReq = {
+          channelNewName: 'NewName',
+          channelUrl: res.body.channelUrl,
+          channelPassword: 'Pass123'
+        };
+        return request(app).put('/').send(editReq).expect(401);
+      });
+  });
+
+  test('no channelNewName sent', () => {
+    db.setDB([]);
+    return request(app).put('/').expect(400);
   });
 });
